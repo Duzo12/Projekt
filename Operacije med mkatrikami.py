b0152650d2ee68e3d0b3e0d4c0a5d4072d248541
Nejc Duščak <nejc.duscak@hotmail.com>
2018-05-21 13:37:40 +0200

    Prva datoteka

---
+++ Operacije med mkatrikami.py
@@ -0,0 +1,49 @@
+def identiteta(n):
+    matrika = []
+    for i in range(n):
+        vrstica = []
+        for j in range(n):
+            vrstica.append(1 if i == j else 0)
+        matrika.append(vrstica)
+    return matrika
+
+def vsota_matrik(a, b):
+    matrika = []
+    if len(a) == len(b) and len(a[0]) == len(b[0]):
+        for i in range(len(a)):
+            vrstica = []
+            for j in range(len(a[i])):
+                vrstica.append(a[i][j] + b[i][j])
+            matrika.append(vrstica)
+        return matrika
+    else: return None
+    
+
+def razlika_matrik(a, b):
+    matrika = []
+    if len(a) == len(b) and len(a[0]) == len(b[0]):
+        for i in range(len(a)):
+            vrstica = []
+            for j in range(len(a[i])):
+                vrstica.append(a[i][j] - b[i][j])
+            matrika.append(vrstica)
+        return matrika
+    else: return None
+
+def produkt_matrik(a, b):
+    matrika = []
+    if len(a[0]) == len(b):
+        for i in range(len(a)):
+            vrstica = []
+            for j in range(len(b)):
+                vsota = 0
+                for k in range(len(a[i])):
+                    vsota += a[i][k] * b[k][j]
+                vrstica.append(vsota)
+            matrika.append(vrstica)
+    return matrika
+                        
+        
+            
+    
+
